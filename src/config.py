43ce7f23962aa1d809da6b801581025cd0ab5c93
speyck <54800864+baltermia@users.noreply.github.com>
2022-04-11 10:55:56 +0200

    Make config.py more static

--- src/config.py
+++ src/config.py
@@ -1,22 +1,17 @@
-
 from configparser import ConfigParser
 
 __configPath = "config.ini"
 
-__config = None
-
 def get_config(path = __configPath):
-    __configPath = path
-
-    if __config is None:
-        __config = ConfigParser()
-        __config.read(path)
+    config = ConfigParser()
+    config.read(path)
     
-    return __config
+    return config
 
 def save_config(config, path = __configPath):
     if config is not ConfigParser:
         return
 
-    __config = config
+    with open(__configPath, "w") as conf:
+        config.write(conf)
     return
