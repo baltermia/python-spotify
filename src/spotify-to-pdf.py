635e7ee1c6c4e4ed7a113cb8d4ac675993791a32
speyck <54800864+baltermia@users.noreply.github.com>
2022-04-11 10:57:11 +0200

    Add calls to spotify module and print raw playlist json

--- src/spotify-to-pdf.py
+++ src/spotify-to-pdf.py
@@ -1,8 +1,12 @@
 import sys
+from spotify import get_playlist
 
 def main(args):
-    pl_id = args[0]
-    email = args[1]
+    pl_id = args[1]
+
+    playlist = get_playlist(pl_id)
+
+    print(playlist)
 
 if __name__ == '__main__':
     main(sys.argv)
