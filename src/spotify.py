6bcc3234a82ccded9b7a565dc96f8cbf639b4be9
speyck <54800864+baltermia@users.noreply.github.com>
2022-04-11 10:27:16 +0200

    Add spotify requests

--- src/spotify.py
+++ src/spotify.py
@@ -1,17 +1,42 @@
 from config import get_config, save_config
+import requests
+from datetime import datetime, timedelta
 
-__config = None
+def get_headers():
+    config = get_config()
+    c_spotify = config["SPOTIFY"]
 
-__token = None
+    if datetime.now() < datetime.strptime(c_spotify["token_expiration"]):
+        return {
+            'Authorization': 'Bearer {}'.format(c_spotify["token"])
+        } 
 
-def __manage_access():
-    client_id = __config["SPOTIFY"]["cid"]
-    client_secret = __config["SPOTIFY"]["secret"]
-    return
+    client_id = c_spotify["cid"]
+    client_secret = c_spotify["secret"]
+
+    data = {
+        'grant_type': 'client_credentials',
+        'client_id': client_id,
+        'client_secret': client_secret,
+    }
+
+    response = requests.post("https://accounts.spotify.com/api/token", data)
+
+    token = response.json().get("access_token")
+    expiration = response.json().get("expires_in")
+
+    c_spotify["token"] = token
+    c_spotify["expiration"] = datetime.now() + timedelta(seconds = expiration)
+
+    save_config()
+
+    return {
+        'Authorization': 'Bearer {}'.format(token)
+    }
 
 def get_playlist(id):
-    __config = get_config()
+    headers = get_headers()
 
-    __manage_access()
+    response = requests.get("https://api.spotify.com/v1/playlists/" + id, headers = headers)
 
-    return
+    return response.json()
