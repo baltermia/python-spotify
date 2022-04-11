6f7bed3635c93dc6fd43ecca414f17a595788589
speyck <54800864+baltermia@users.noreply.github.com>
2022-04-11 11:27:39 +0200

    Get playlist as pdf and write it into tmp file 'out.pdf'

--- src/spotify-to-pdf.py
+++ src/spotify-to-pdf.py
@@ -1,12 +1,18 @@
 import sys
+from venv import create
 from spotify import get_playlist
+from pdf import create_pdf
+import codecs
 
 def main(args):
     pl_id = args[1]
 
     playlist = get_playlist(pl_id)
 
-    print(playlist)
+    pdf = create_pdf(playlist)
+
+    with open("out.pdf", "wb") as f:
+        f.write(pdf)
 
 if __name__ == '__main__':
     main(sys.argv)
