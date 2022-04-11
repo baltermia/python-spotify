1cd1ebf41ba8f2c1ab54ba979d870491b8889b94
speyck <54800864+baltermia@users.noreply.github.com>
2022-04-11 11:11:18 +0200

    Create first empty page

--- src/pdf.py
+++ src/pdf.py
@@ -1,4 +1,9 @@
 from fpdf import FPDF
 
 def create_pdf(json):
+    # Create default pdf setttings and add first page
     pdf = FPDF()
+    pdf.set_font('Helvetica', '', 16)
+    pdf.add_page()
+    pdf.image('src/res/background.png', x = 0, y = 0, w = 210, h = 297, type = '', link = '')
+    pdf.set_text_color(255, 255, 255)
