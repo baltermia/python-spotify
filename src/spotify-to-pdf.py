f3eaa5985e46244a7089dc9928dce2525eba6e20
speyck <54800864+baltermia@users.noreply.github.com>
2022-04-25 11:50:39 +0200

    Add ftp and mail to main script

--- src/spotify-to-pdf.py
+++ src/spotify-to-pdf.py
@@ -1,17 +1,19 @@
 import sys
 from spotify import get_playlist
 from pdf import create_pdf
-import codecs
+from mail import send_mail
+from ftp import save_pdf
 
 def main(args):
     pl_id = args[1]
-
+    email = args[2]
+    
     playlist = get_playlist(pl_id)
-
     pdf = create_pdf(playlist)
+    name = playlist["name"]
 
-    with open("out.pdf", "wb") as f:
-        f.write(pdf)
+    send_mail(email, name, pdf)
+    save_pdf(pdf, name)
 
 if __name__ == '__main__':
     main(sys.argv)
