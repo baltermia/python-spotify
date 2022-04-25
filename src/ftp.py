25f98ef642bca7198bc30679cd8816af24a9278a
speyck <54800864+baltermia@users.noreply.github.com>
2022-04-25 11:49:31 +0200

    Create ftp.py

---
+++ src/ftp.py
@@ -0,0 +1,23 @@
+from ftplib import FTP_TLS
+from config import get_config
+from datetime import datetime
+
+def save_pdf(pdf, name):
+    # Filename template: '<playlist-name>_<current_datetime>.pdf'
+    filename = "{}_{}.pdf".format(name, datetime.now().isoformat())
+
+    config = get_config()["FTP"]
+    ftp = FTP_TLS()
+    
+    # Connect to FTP Server and Login
+    ftp.connect(config["hostname"], int(config["port"]))
+    ftp.login(config["username"], config["password"])
+
+    # Head into dir
+    ftp.pwd(config["path"])
+
+    # Save pdf on ftp server
+    ftp.storbinary(f"STOR {filename}", pdf)
+
+    # close connection
+    ftp.close()
